be2a4dd1242e59f67c8b902bde8d66fb43c44df9
Yufan Zhang <yz605@duke.edu>
2022-07-10 23:32:19 +0800

    Update gitignore

--- test.sh
+++ test.sh
@@ -1,2 +1,11 @@
 set -ex
-python test.py --dataroot ./datasets/font  --model mfnet --dataset_mode multisource --eval --name mfnet_unseen_lang --no_dropout --gpu_id -1 --phase mfnet_unseen_lang_test
+
+for ((i=1; i<=20; i ++))
+do
+    starttime=`date +%s`
+    python test.py --dataroot ./datasets/font  --model mfnet --dataset_mode font --eval --name mfnet_unseen_lang --no_dropout --gpu_id -1 --phase mfnet_unseen_lang_test --epoch $i
+    endtime=`date +%s`
+    # start_seconds=$(data --date="$starttime" +%s);
+    # end_seconds=$(data --date="$endtime" +%s);
+    echo "Inferring time: "$((endtime-starttime))"s" >> "./results/epoch_${i}_time.txt"
+done
